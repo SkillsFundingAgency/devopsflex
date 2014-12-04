6c037792366d2fccc0ffda787656e3df1d2b88ca
djfr <davidferreirarodrigues@gmail.com>
2014-12-04 14:59:55 +0000

    Added the NuGet spec and made sure that the NuGet package isn't added to Git

--- DevOpsFlex/DevOpsFlex.Activities/Properties/AssemblyInfo.cs
+++ DevOpsFlex/DevOpsFlex.Activities/Properties/AssemblyInfo.cs
@@ -1,37 +1,18 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("DevOpsFlex")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("DevOps support for TFS driven teams using TFS Build, Release Management and other components on the Microsoft Stack.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Skills Funding Agency (UK)")]
 [assembly: AssemblyProduct("DevOpsFlex")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6ee8dc3c-6116-4f1c-8538-4f2dbc390666")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
