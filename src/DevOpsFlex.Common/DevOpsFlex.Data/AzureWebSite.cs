b796f8943453bbf124ad67796e9a762707cde7a5
djfr <davidferreirarodrigues@gmail.com>
2015-04-13 15:01:46 +0100

    Added Azure Web Sites to the model. Added the seed FCT layout for web sites.

---
+++ src/DevOpsFlex.Common/DevOpsFlex.Data/AzureWebSite.cs
@@ -0,0 +1,13 @@
+﻿namespace DevOpsFlex.Data
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class AzureWebSite : DevOpsComponent
+    {
+        [Required, MaxLength(500)]
+        public string PublishProjectTfsPath { get; set; }
+
+        [Required, MaxLength(500)]
+        public string SolutionTfsPath { get; set; }
+    }
+}
