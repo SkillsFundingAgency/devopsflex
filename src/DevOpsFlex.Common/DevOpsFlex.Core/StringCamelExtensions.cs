fd04cdaacabf95a95591e028695cf878d386a1a0
djfr <davidferreirarodrigues@gmail.com>
2015-04-14 11:14:27 +0100

    Added code contracts validation to the GetUpperConcat method. Added unit tests that cover 100% of GetUpperConcat, including the code contracts.

--- src/DevOpsFlex.Common/DevOpsFlex.Core/StringCamelExtensions.cs
+++ src/DevOpsFlex.Common/DevOpsFlex.Core/StringCamelExtensions.cs
@@ -1,11 +1,23 @@
 ﻿namespace DevOpsFlex.Core
 {
+    using System.Diagnostics.Contracts;
     using System.Linq;
 
+    /// <summary>
+    /// Contains string extension methods that help with camel case naming conventions.
+    /// </summary>
     public static class StringCamelExtensions
     {
+        /// <summary>
+        /// Gets the uppercase letters on a string as lower case. Used to minimalize camel names.
+        /// </summary>
+        /// <param name="value">The input string.</param>
+        /// <returns>The uppercase letters on a string as lower case.</returns>
         public static string GetUpperConcat(this string value)
         {
+            Contract.Requires(!string.IsNullOrWhiteSpace(value));
+            Contract.Ensures(!string.IsNullOrWhiteSpace(Contract.Result<string>()));
+
             return string.Join("", value.ToCharArray().Where(char.IsUpper))
                          .ToLower();
         }
