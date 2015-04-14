5156ceb89735f471d58c92bc0ee8b87d09821290
djfr <davidferreirarodrigues@gmail.com>
2015-04-14 11:14:27 +0100

    Added code contracts validation to the GetUpperConcat method. Added unit tests that cover 100% of GetUpperConcat, including the code contracts.

--- src/DevOpsFlex.Common/Tests/DevOpsFlex.Core.Tests/StringCamelExtensionsTest.cs
+++ src/DevOpsFlex.Common/Tests/DevOpsFlex.Core.Tests/StringCamelExtensionsTest.cs
@@ -1,11 +1,21 @@
 ﻿namespace DevOpsFlex.Core.Tests
 {
+    using System;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+    /// <summary>
+    /// Constains tests that target the camel casing string extensions class.
+    /// </summary>
     [TestClass]
     public class StringCamelExtensionsTest
     {
-        [TestMethod]
+
+        #region GetUpperConcat
+
+        /// <summary>
+        /// Tests the GetUpperConcat with the "FundingStreamConfig" string.
+        /// </summary>
+        [TestMethod, TestCategory("Unit")]
         public void Test_GetUpperConcat_WithThreeWords()
         {
             const string input = "FundingStreamConfig";
@@ -14,5 +24,101 @@
 
             Assert.AreEqual(result, "fsc");
         }
+
+        /// <summary>
+        /// Tests the GetUpperConcat with the "BT" string.
+        /// </summary>
+        [TestMethod, TestCategory("Unit")]
+        public void Test_GetUpperConcat_WithOnlyTwoUpperLetters()
+        {
+            const string input = "BT";
+
+            var result = input.GetUpperConcat();
+
+            Assert.AreEqual(result, "bt");
+        }
+
+        /// <summary>
+        /// Tests the GetUpperConcat with a null string.
+        /// </summary>
+        /// <remarks>
+        /// This test validates a Code Contract, so it will only pass if run with Code Contracts rewrite (usually Debug).
+        /// </remarks>
+        [TestMethod, TestCategory("Unit")]
+        public void Ensure_GetUpperConcat_ValidatesNull()
+        {
+            try
+            {
+                StringCamelExtensions.GetUpperConcat(null);
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(ex.GetType().Name, "ContractException");
+                StringAssert.Contains(ex.Message.ToLower(), "precondition failed:");
+            }
+        }
+
+        /// <summary>
+        /// Tests the GetUpperConcat with an empty string.
+        /// </summary>
+        /// <remarks>
+        /// This test validates a Code Contract, so it will only pass if run with Code Contracts rewrite (usually Debug).
+        /// </remarks>
+        [TestMethod, TestCategory("Unit")]
+        public void Ensure_GetUpperConcat_ValidatesEmpty()
+        {
+            try
+            {
+                "".GetUpperConcat();
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(ex.GetType().Name, "ContractException");
+                StringAssert.Contains(ex.Message.ToLower(), "precondition failed:");
+            }
+        }
+
+        /// <summary>
+        /// Tests the GetUpperConcat with a string that only contains white space.
+        /// </summary>
+        /// <remarks>
+        /// This test validates a Code Contract, so it will only pass if run with Code Contracts rewrite (usually Debug).
+        /// </remarks>
+        [TestMethod, TestCategory("Unit")]
+        public void Ensure_GetUpperConcat_ValidatesWhiteSpace()
+        {
+            try
+            {
+                " ".GetUpperConcat();
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(ex.GetType().Name, "ContractException");
+                StringAssert.Contains(ex.Message.ToLower(), "precondition failed:");
+            }
+        }
+
+        /// <summary>
+        /// Tests the GetUpperConcat with a string that only contains lower case, thus returning an empty string.
+        /// </summary>
+        /// <remarks>
+        /// This test validates a Code Contract, so it will only pass if run with Code Contracts rewrite (usually Debug).
+        /// </remarks>
+        [TestMethod, TestCategory("Unit")]
+        public void Ensure_GetUpperConcat_ValidatesBlankReturn()
+        {
+            try
+            {
+                "fsc".GetUpperConcat();
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(ex.GetType().Name, "ContractException");
+                StringAssert.Contains(ex.Message.ToLower(), "postcondition failed:");
+            }
+        }
+
+        #endregion
+
     }
 }
