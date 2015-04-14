70eed092571915cbd028f82e0c936c11849112ef
djfr <davidferreirarodrigues@gmail.com>
2015-04-14 11:31:51 +0100

    Added unit tests that cover 100% of the GetEnumExtensions mapping method.

--- src/DevOpsFlex.Common/DevOpsFlex.Core/EnumExtensions.cs
+++ src/DevOpsFlex.Common/DevOpsFlex.Core/EnumExtensions.cs
@@ -3,8 +3,17 @@
     using System;
     using System.ComponentModel;
 
+    /// <summary>
+    /// Contains methods that extend the enum struct with utility and mapping methods.
+    /// </summary>
     public static class EnumExtensions
     {
+        /// <summary>
+        /// Gets the description attribute value associated with an enum value, or if not present
+        /// returns the enum value ToString() value.
+        /// </summary>
+        /// <param name="value">The enum value we want to get the description from.</param>
+        /// <returns>The value of the description attribute present in this enum value.</returns>
         public static string GetEnumDescription(this Enum value)
         {
             var attributes =
---
+++ src/DevOpsFlex.Common/Tests/DevOpsFlex.Core.Tests/EnumExtensionsTest.cs
@@ -0,0 +1,54 @@
+﻿namespace DevOpsFlex.Core.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Contains tests that target the <see cref="EnumExtensions"/> utility/mapping class for enums.
+    /// </summary>
+    [TestClass]
+    public class EnumExtensionsTest
+    {
+
+        #region GetEnumDescription
+
+        /// <summary>
+        /// Holds a constant to the description attribute for the GetEnumDescription tests.
+        /// </summary>
+        private const string EnumDescription = "Some Description";
+
+        /// <summary>
+        /// Tests GetEnumDescription with an enum value that contains the <see cref="System.ComponentModel.DescriptionAttribute"/>.
+        /// </summary>
+        [TestMethod, TestCategory("Unit")]
+        public void Test_GetEnumDescription_WithAValidDescription()
+        {
+            var result = SomeEnumWithDescription.HasDescription.GetEnumDescription();
+
+            Assert.AreEqual(result, EnumDescription);
+        }
+
+        /// <summary>
+        /// Tests GetEnumDescription with an enum value that does not contain the <see cref="System.ComponentModel.DescriptionAttribute"/>.
+        /// Ensures that the ToString() representation is returned instead.
+        /// </summary>
+        [TestMethod, TestCategory("Unit")]
+        public void Test_GetEnumDescription_WithNoDescription()
+        {
+            var result = SomeEnumWithDescription.NoDescription.GetEnumDescription();
+
+            Assert.AreEqual(result, SomeEnumWithDescription.NoDescription.ToString());
+        }
+
+        /// <summary>
+        /// Utility enum for the GetEnumDescription tests.
+        /// </summary>
+        enum SomeEnumWithDescription
+        {
+            [System.ComponentModel.Description(EnumDescription)] HasDescription,
+            NoDescription
+        }
+
+        #endregion
+
+    }
+}
