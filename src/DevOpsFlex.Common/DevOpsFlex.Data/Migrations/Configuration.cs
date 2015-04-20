6bf781ea7c9f21e1af58afe8672f02414a4d0693
djfr <davidferreirarodrigues@gmail.com>
2015-04-20 11:10:51 +0100

    Initial draft of the provisioning of SQL databases. Added a model object for Azure SQL DB firewall rules. Added the check and creation of SQL databases.

--- src/DevOpsFlex.Common/DevOpsFlex.Data/Migrations/Configuration.cs
+++ src/DevOpsFlex.Common/DevOpsFlex.Data/Migrations/Configuration.cs
@@ -11,7 +11,14 @@ namespace DevOpsFlex.Data.Migrations
 
         protected override void Seed(DevOpsFlexDbContext context)
         {
-            var fctSystem =
+            var fctSystem = SeedSystem(context);
+            SeedComponents(context, fctSystem);
+            SeedFirewallRules(context, fctSystem);
+        }
+
+        private static DevOpsSystem SeedSystem(DevOpsFlexDbContext context)
+        {
+            var system =
                 new DevOpsSystem
                 {
                     AfinityGroup = "FCTWest",
@@ -21,13 +28,18 @@ namespace DevOpsFlex.Data.Migrations
                     LogicalName = "FCT"
                 };
 
-            context.Systems.AddOrUpdate(s => s.Name, fctSystem);
+            context.Systems.AddOrUpdate(s => s.LogicalName, system);
 
+            return system;
+        }
+
+        private static void SeedComponents(DevOpsFlexDbContext context, DevOpsSystem system)
+        {
             context.Components.AddOrUpdate(
                 c => c.LogicalName,
                 new AzureCloudService
                 {
-                    System = fctSystem,
+                    System = system,
                     LogicalName = "ServiceBus",
                     Name = "CrossDomain.Integration NServiceBus role",
                     Label = "Main NServiceBus role with most of the endpoints",
@@ -36,7 +48,7 @@ namespace DevOpsFlex.Data.Migrations
                 },
                 new AzureCloudService
                 {
-                    System = fctSystem,
+                    System = system,
                     LogicalName = "OrgService",
                     Name = "Organisation service",
                     Label = "Organisation service",
@@ -45,7 +57,7 @@ namespace DevOpsFlex.Data.Migrations
                 },
                 new AzureCloudService
                 {
-                    System = fctSystem,
+                    System = system,
                     LogicalName = "FundingStreamConfig",
                     Name = "Funding Stream Config service",
                     Label = "Funding Stream Config service",
@@ -54,27 +66,27 @@ namespace DevOpsFlex.Data.Migrations
                 },
                 new SqlAzureDb
                 {
-                    System = fctSystem,
+                    System = system,
                     LogicalName = "ServiceBusDb",
                     Name = "NServiceBus persistency database",
-                    Edition = SqlAzureEdition.Business,
+                    Edition = SqlAzureEdition.Standard,
                     DTUs = 10,
                     MaximumDatabaseSizeInGB = 10,
                     CollationName = "SQL_Latin1_General_CP1_CI_AS"
                 },
                 new SqlAzureDb
                 {
-                    System = fctSystem,
+                    System = system,
                     LogicalName = "OrganisationDb",
                     Name = "Organisation service persistency database",
-                    Edition = SqlAzureEdition.Business,
+                    Edition = SqlAzureEdition.Standard,
                     DTUs = 10,
                     MaximumDatabaseSizeInGB = 10,
                     CollationName = "SQL_Latin1_General_CP1_CI_AS"
                 },
                 new AzureWebSite
                 {
-                    System = fctSystem,
+                    System = system,
                     LogicalName = "MocksSite",
                     Name = "Mocks services (Stubs)",
                     PublishProjectTfsPath = "$/FCT/Main/Mocks/FctServices/FctServices.publish.proj",
@@ -82,7 +94,7 @@ namespace DevOpsFlex.Data.Migrations
                 },
                 new AzureWebSite
                 {
-                    System = fctSystem,
+                    System = system,
                     LogicalName = "WebJobsSite",
                     Name = "Web Jobs hosting",
                     PublishProjectTfsPath = "$/FCT/Main/CrossDomain.Integration/CrossDomain.Integration.Azure.WebJobsHostSite.publish.proj",
@@ -90,18 +102,38 @@ namespace DevOpsFlex.Data.Migrations
                 },
                 new AzureServiceBusNamespace
                 {
-                    System = fctSystem,
+                    System = system,
                     LogicalName = "IntegrationSb",
                     Name = "NServiceBus main namespace",
                     Region = ServiceBusRegions.NorthEurope
                 },
                 new AzureStorageContainer
                 {
-                    System = fctSystem,
+                    System = system,
                     LogicalName = "DataBusStorage",
                     Name = "NServiceBus DataBus storage",
                     ExternalAccess = ContainerExternalAccess.Private,
                     Acl = ContainerAcl.Read | ContainerAcl.Write
+                });
+        }
+
+        private static void SeedFirewallRules(DevOpsFlexDbContext context, DevOpsSystem system)
+        {
+            context.SqlFirewallRules.AddOrUpdate(
+                r => r.Name,
+                new SqlFirewallRule
+                {
+                    System = system,
+                    Name = "SFA LAN",
+                    StartIp = "193.240.137.225",
+                    EndIp = "193.240.137.254"
+                },
+                new SqlFirewallRule
+                {
+                    System = system,
+                    Name = "Cap UK",
+                    StartIp = "212.167.5.1",
+                    EndIp = "212.167.5.255"
                 });
         }
     }
---
+++ src/DevOpsFlex.Common/Tests/DevOpsFlex.Azure.Management.Tests/SqlManagementClientExtensionsTest.cs
@@ -0,0 +1,60 @@
+﻿namespace DevOpsFlex.Azure.Management.Tests
+{
+    using System;
+    using System.Linq;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Threading.Tasks;
+    using Core;
+    using Data;
+    using Data.PublishSettings;
+    using Microsoft.Azure;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Microsoft.WindowsAzure.Management.Sql;
+
+    [TestClass]
+    public class SqlManagementClientExtensionsTest
+    {
+
+        #region Integration Tests
+
+        /// <summary>
+        /// Specifies the relative or absolute path to the publish settings file for the target subscription.
+        /// </summary>
+        private const string SettingsPath = @"C:\PublishSettings\sfa_beta.publishsettings";
+
+        /// <summary>
+        /// Specifies the subscription Id that we want to target.
+        /// This subscription needs to be defined and found in the publish settings file.
+        /// </summary>
+        private const string SubscriptionId = "102d951b-78c0-4e48-80d4-a9c13baca2ad";
+
+        [TestMethod]
+        public async Task Foo()
+        {
+            using (var client = CreateClient())
+            {
+                await client.CheckCreateDatabase("acx03vg6p0", "fct-db123", SqlAzureEdition.Standard.GetEnumDescription(), "SQL_Latin1_General_CP1_CI_AS", 5);
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Creates a standard <see cref="SqlManagementClient"/> that targets the Azure subscription
+        /// specified in the <see cref="AzureSubscription"/> static class.
+        /// </summary>
+        /// <returns>
+        /// A standard <see cref="SqlManagementClient"/> that targets the Azure subscription
+        /// specified in the <see cref="AzureSubscription"/> static class.
+        /// </returns>
+        private static SqlManagementClient CreateClient()
+        {
+            var azureSubscription = new AzureSubscription(SettingsPath, SubscriptionId);
+
+            return new SqlManagementClient(
+                new CertificateCloudCredentials(
+                    SubscriptionId,
+                    new X509Certificate2(Convert.FromBase64String(azureSubscription.ManagementCertificate))));
+        }
+    }
+}
