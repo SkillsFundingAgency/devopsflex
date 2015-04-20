5c44082edce3858d86bbd846268fc786ccf2d7c1
djfr <davidferreirarodrigues@gmail.com>
2015-04-20 21:55:49 +0100

    Added a common unit tests utility/support project with useful test objects and extensions.

---
+++ src/DevOpsFlex.Common/Tests/UnitTests.Common/EnumExtensions.cs
@@ -0,0 +1,34 @@
+﻿namespace UnitTests.Common
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Provides a set of helper methods or extensions that help to deal
+    /// with enums in the context of a unit test.
+    /// </summary>
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// Checks if 2 enums have the same elements with the same enum values.
+        /// </summary>
+        /// <typeparam name="T1">The First enum to check for.</typeparam>
+        /// <typeparam name="T2">The Second enum to check for.</typeparam>
+        /// <returns>A list of fully qualified name values of enum differences.</returns>
+        public static IEnumerable<string> EnumDifferences<T1, T2>()
+            where T1 : struct
+            where T2 : struct
+        {
+            var enumOneNames = Enum.GetNames(typeof(T1));
+            var enumTwoNames = Enum.GetNames(typeof(T2));
+            var intersected = enumOneNames.Intersect(enumTwoNames).ToList();
+
+            var oneExceptions = enumOneNames.Except(intersected);
+            var twoExceptions = enumTwoNames.Except(intersected);
+
+            return oneExceptions.Select(e => typeof(T1).FullName + "." + e)
+                                .Concat(twoExceptions.Select(e => typeof(T2).FullName + "." + e));
+        }
+    }
+}
---
+++ src/DevOpsFlex.Common/Tests/UnitTests.Common/TestResource.cs
@@ -0,0 +1,80 @@
+﻿namespace UnitTests.Common
+{
+    using System;
+    using System.IO;
+    using System.Reflection;
+
+    /// <summary>
+    /// Wraps an embed resource around a class that can materialize it to file system
+    /// and delete on the call to Dispose, to wrap it on a using block.
+    /// </summary>
+    public class TestResource : IDisposable
+    {
+        /// <summary>
+        /// Initializes a Resource wrapper based on a namespace and a resource name.
+        /// </summary>
+        /// <param name="baseNamespace">The namespace of the resource.</param>
+        /// <param name="resourceName">The name of the resource.</param>
+        public TestResource(string baseNamespace, string resourceName)
+        {
+            using (var resourceStream = Assembly.GetCallingAssembly()
+                                                .GetManifestResourceStream(baseNamespace + "." + resourceName))
+            {
+                if (resourceStream == null)
+                {
+                    throw new ArgumentException(
+                        string.Format(
+                            "Invalid fully qualified name of the Resource : {0}.{1}",
+                            baseNamespace,
+                            resourceName));
+                }
+
+                using (var fileStream = File.Create(resourceName))
+                {
+                    resourceStream.CopyTo(fileStream);
+                }
+
+                ResourceName = resourceName;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the resource name.
+        /// </summary>
+        public string ResourceName { get; set; }
+
+        /// <summary>
+        /// Gets the full path where this resource should be materialized to.
+        /// </summary>
+        public string FullPath
+        {
+            get { return Path.Combine(Environment.CurrentDirectory, ResourceName); }
+        }
+
+        /// <summary>
+        /// Defines a method to release allocated resources.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Defines a method to release allocated resources.
+        /// </summary>
+        /// <param name="disposing">If we are already disposing or not.</param>
+        private void Dispose(bool disposing)
+        {
+            if (!disposing)
+            {
+                return;
+            }
+
+            if (ResourceName != null)
+            {
+                File.Delete(ResourceName);
+            }
+        }
+    }
+}
