928db4a1369d008751fa07d9964cb647cf3941f6
djfr <davidferreirarodrigues@gmail.com>
2015-04-23 15:54:16 +0100

    Added a check create database method that just needs the data model to create a database.

--- src/DevOpsFlex.Common/DevOpsFlex.Azure.Management/SqlManagementClientExtensions.cs
+++ src/DevOpsFlex.Common/DevOpsFlex.Azure.Management/SqlManagementClientExtensions.cs
@@ -1,12 +1,28 @@
 ﻿namespace DevOpsFlex.Azure.Management
 {
     using System.Threading.Tasks;
+    using Core;
+    using Data;
     using Hyak.Common;
     using Microsoft.WindowsAzure.Management.Sql;
     using Microsoft.WindowsAzure.Management.Sql.Models;
 
+    /// <summary>
+    /// Extends the <see cref="SqlManagementClient"/> with usefull extensions that the devopsflex
+    /// activities and commandlets need in order to achieve their execution.
+    /// </summary>
     public static class SqlManagementClientExtensions
     {
+        /// <summary>
+        /// Checks for the existence of a specific Azure Sql Database, if it doesn't exist it will create it.
+        /// </summary>
+        /// <param name="client">The <see cref="SqlManagementClient"/> that is performing the operation.</param>
+        /// <param name="serverName">The name of the server that we want to use to create the database.</param>
+        /// <param name="databaseName">The name of the database we are creating.</param>
+        /// <param name="databaseEdition">The edition of the database we are creating.</param>
+        /// <param name="collationName">The database collation name.</param>
+        /// <param name="sizeInGb">The maximum database size in GB.</param>
+        /// <returns>The async <see cref="Task"/> wrapper.</returns>
         public static async Task CheckCreateDatabase(
             this SqlManagementClient client,
             string serverName,
@@ -37,6 +53,22 @@
                     CollationName = collationName,
                     MaximumDatabaseSizeInGB = sizeInGb
                 });
+        }
+
+        /// <summary>
+        /// Checks for the existence of a specific Azure Sql Database, if it doesn't exist it will create it.
+        /// </summary>
+        /// <param name="client">The <see cref="SqlManagementClient"/> that is performing the operation.</param>
+        /// <param name="model">The DevOpsFlex rich model object that contains everything there is to know about this database spec.</param>
+        /// <returns>The async <see cref="Task"/> wrapper.</returns>
+        public static async Task CheckCreateDatabase(this SqlManagementClient client, SqlAzureDb model)
+        {
+            await client.CheckCreateDatabase(
+                await FlexConfiguration.SqlServerChooser.Choose(client, model.System.Location.GetEnumDescription()),
+                FlexConfiguration.GetNaming<SqlAzureDb>().GetSlotName(model),
+                model.Edition.GetEnumDescription(),
+                model.CollationName,
+                model.MaximumDatabaseSizeInGB);
         }
     }
 }
