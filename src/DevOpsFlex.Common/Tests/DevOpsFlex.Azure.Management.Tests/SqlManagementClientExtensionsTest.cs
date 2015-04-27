c0b671095317fc2b5e7a643798034fa2f020013a
djfr <davidferreirarodrigues@gmail.com>
2015-04-27 10:41:46 +0100

    Added two integration tests to the SQL client to validate the behaviour with existing resources.

--- src/DevOpsFlex.Common/Tests/DevOpsFlex.Azure.Management.Tests/SqlManagementClientExtensionsTest.cs
+++ src/DevOpsFlex.Common/Tests/DevOpsFlex.Azure.Management.Tests/SqlManagementClientExtensionsTest.cs
@@ -27,6 +27,10 @@
         [TestMethod, TestCategory("Integration")]
         public async Task Test_CheckCreateDatabase_WithNewDatabase()
         {
+            const int dbSize = 5;
+            const string dbCollation = "SQL_Latin1_General_CP1_CI_AS";
+            var dbEdition = SqlAzureEdition.Standard.GetEnumDescription();
+
             using (var client = ManagementClient.CreateSqlClient())
             {
                 var dbName = "fct-" + Guid.NewGuid().ToString().Split('-').Last();
@@ -38,10 +42,51 @@
 
                 try
                 {
-                    await client.CheckCreateDatabase(server, dbName, SqlAzureEdition.Standard.GetEnumDescription(), "SQL_Latin1_General_CP1_CI_AS", 5);
+                    await client.CheckCreateDatabase(server, dbName, dbEdition, dbCollation, dbSize);
 
                     var webSite = await client.Databases.GetAsync(server, dbName, new CancellationToken());
                     Assert.IsNotNull(webSite);
+                }
+                finally
+                {
+                    client.Databases.Delete(server, dbName);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Tests CheckCreateDatabase with the creation of a new database that already exists in the server.
+        /// </summary>
+        [TestMethod, TestCategory("Integration")]
+        public async Task Test_CheckCreateDatabase_WithExistingDatabase()
+        {
+            const int dbSize = 5;
+            const string dbCollation = "SQL_Latin1_General_CP1_CI_AS";
+            var dbEdition = SqlAzureEdition.Standard.GetEnumDescription();
+
+            using (var client = ManagementClient.CreateSqlClient())
+            {
+                var dbName = "fct-" + Guid.NewGuid().ToString().Split('-').Last();
+
+                var chooser = new DefaultAzureSqlServerChooser();
+
+                var server = await chooser.Choose(client, SystemLocation.WestEurope.GetEnumDescription());
+                Assert.IsFalse(string.IsNullOrWhiteSpace(server));
+
+                try
+                {
+                    await client.Databases.CreateAsync(
+                        server,
+                        new DatabaseCreateParameters
+                        {
+                            Name = dbName,
+                            Edition = dbEdition,
+                            CollationName = dbCollation,
+                            MaximumDatabaseSizeInGB = dbSize
+
+                        });
+
+                    await client.CheckCreateDatabase(server, dbName, dbEdition, dbCollation, dbSize);
                 }
                 finally
                 {
@@ -57,6 +102,9 @@
         [TestMethod, TestCategory("Integration")]
         public async Task Test_CheckCreateFirewallRule_WithNewFirewallRule()
         {
+            const string startIp = "111.111.111.111";
+            const string endIp = "111.111.111.111";
+
             using (var client = ManagementClient.CreateSqlClient())
             {
                 var ruleName = "fct-" + Guid.NewGuid().ToString().Split('-').Last();
@@ -68,15 +116,63 @@
 
                 try
                 {
-                    await client.CheckCreateFirewallRule(server, new FirewallRuleCreateParameters
-                    {
-                        Name = ruleName,
-                        StartIPAddress = "111.111.111.111",
-                        EndIPAddress = "111.111.111.111"
-                    });
+                    await client.CheckCreateFirewallRule(
+                        server,
+                        new FirewallRuleCreateParameters
+                        {
+                            Name = ruleName,
+                            StartIPAddress = startIp,
+                            EndIPAddress = endIp
+                        });
 
                     var rule = await client.FirewallRules.GetAsync(server, ruleName);
                     Assert.IsNotNull(rule);
+                }
+                finally
+                {
+                    client.FirewallRules.Delete(server, ruleName);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Tests CheckCreateFirewallRule with the creation of a new firewall rule that already exists in the server.
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod, TestCategory("Integration")]
+        public async Task Test_CheckCreateFirewallRule_WithExistingFirewallRule()
+        {
+            const string startIp = "111.111.111.111";
+            const string endIp = "111.111.111.111";
+
+            using (var client = ManagementClient.CreateSqlClient())
+            {
+                var ruleName = "fct-" + Guid.NewGuid().ToString().Split('-').Last();
+
+                var chooser = new DefaultAzureSqlServerChooser();
+
+                var server = await chooser.Choose(client, SystemLocation.WestEurope.GetEnumDescription());
+                Assert.IsFalse(string.IsNullOrWhiteSpace(server));
+
+                try
+                {
+                    await client.FirewallRules.CreateAsync(
+                        server,
+                        new FirewallRuleCreateParameters
+                        {
+                            Name = ruleName,
+                            StartIPAddress = startIp,
+                            EndIPAddress = endIp
+                        });
+
+                    await client.CheckCreateFirewallRule(
+                        server,
+                        new FirewallRuleCreateParameters
+                        {
+                            Name = ruleName,
+                            StartIPAddress = startIp,
+                            EndIPAddress = endIp
+                        });
                 }
                 finally
                 {
