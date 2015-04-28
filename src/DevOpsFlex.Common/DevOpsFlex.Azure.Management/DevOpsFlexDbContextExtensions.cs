d5001463ae3abe7c4457165ef80c5f948fbf3a70
djfr <davidferreirarodrigues@gmail.com>
2015-04-28 13:21:05 +0100

    Added service bus namespaces provisioning at the IQueryable level and created an integration test that overrides naming with the legacy FCT naming convention for service bus.

--- src/DevOpsFlex.Common/DevOpsFlex.Azure.Management/DevOpsFlexDbContextExtensions.cs
+++ src/DevOpsFlex.Common/DevOpsFlex.Azure.Management/DevOpsFlexDbContextExtensions.cs
@@ -1,10 +1,13 @@
 ﻿namespace DevOpsFlex.Azure.Management
 {
+    using System.Collections.Generic;
+    using System.Data.Entity;
     using System.Linq;
     using System.Threading.Tasks;
     using Data;
     using Microsoft.WindowsAzure.Management.Compute;
     using Microsoft.WindowsAzure.Management.Network;
+    using Microsoft.WindowsAzure.Management.ServiceBus;
 
     /// <summary>
     /// Extends the <see cref="DevOpsFlexDbContext"/> with usefull extensions that the devopsflex
@@ -17,19 +20,19 @@
     public static class DevOpsFlexDbContextExtensions
     {
         /// <summary>
-        /// Provisions all the services in the <see cref="IQueryable"/> of <see cref="AzureCloudService"/>.
+        /// Provisions all the services in the <see cref="IEnumerable{T}"/> of <see cref="AzureCloudService"/>.
         /// </summary>
         /// <param name="services">The list of <see cref="AzureCloudService"/> to provision.</param>
         /// <param name="client">The <see cref="ComputeManagementClient"/> that is performing the operation.</param>
         /// <returns>The async <see cref="Task"/> wrapper.</returns>
         public static async Task ProvisionAll(this IQueryable<AzureCloudService> services, ComputeManagementClient client)
         {
-            var tasks = services.ToList()
-                                .Select(
-                                    async s =>
-                                    {
-                                        await client.CheckCreateCloudService(s);
-                                    });
+            var tasks = (await services.ToListAsync())
+                .Select(
+                    async s =>
+                    {
+                        await client.CheckCreateCloudService(s);
+                    });
 
             await Task.WhenAll(tasks);
         }
@@ -42,13 +45,30 @@
         /// <returns>The async <see cref="Task"/> wrapper.</returns>
         public static async Task ReserveAllIps(this IQueryable<AzureCloudService> services, NetworkManagementClient client)
         {
-            var tasks = services.Where(s => s.ReserveIp)
-                                .ToList()
-                                .Select(
-                                    async s =>
-                                    {
-                                        await client.CheckCreateReservedIp(s);
-                                    });
+            var tasks = (await services.Where(s => s.ReserveIp).ToListAsync())
+                .Select(
+                    async s =>
+                    {
+                        await client.CheckCreateReservedIp(s);
+                    });
+
+            await Task.WhenAll(tasks);
+        }
+
+        /// <summary>
+        /// Provisions all the namespaces in the <see cref="IEnumerable{T}"/> of <see cref="AzureServiceBusNamespace"/>.
+        /// </summary>
+        /// <param name="namespaces">The list of <see cref="AzureServiceBusNamespace"/> to provision.</param>
+        /// <param name="client">The <see cref="ServiceBusManagementClient"/> that is performing the operation.</param>
+        /// <returns>The async <see cref="Task"/> wrapper.</returns>
+        public static async Task ProvisionAll(this IQueryable<AzureServiceBusNamespace> namespaces, ServiceBusManagementClient client)
+        {
+            var tasks = (await namespaces.ToListAsync())
+                .Select(
+                    async n =>
+                    {
+                        await client.CheckCreateNamespace(n);
+                    });
 
             await Task.WhenAll(tasks);
         }
--- src/DevOpsFlex.Common/Tests/DevOpsFlex.Azure.Management.Tests/DevOpsFlexDbContextExtensionsTest.cs
+++ src/DevOpsFlex.Common/Tests/DevOpsFlex.Azure.Management.Tests/DevOpsFlexDbContextExtensionsTest.cs
@@ -3,6 +3,7 @@
     using System.Linq;
     using System.Threading.Tasks;
     using Data;
+    using Data.Naming;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     /// <summary>
@@ -18,7 +19,7 @@
         public async Task Test_AzureCloudService_ProvisionAll_End2End()
         {
             FlexDataConfiguration.Branch = "Main";
-            FlexDataConfiguration.Configuration = "djfr";
+            FlexDataConfiguration.Configuration = "MO";
 
             using (var client = ManagementClient.CreateComputeClient())
             using (var context = new DevOpsFlexDbContext())
@@ -34,13 +35,41 @@
         public async Task Test_AzureCloudService_ReserveAllIps_End2End()
         {
             FlexDataConfiguration.Branch = "Main";
-            FlexDataConfiguration.Configuration = "djfr";
+            FlexDataConfiguration.Configuration = "MO";
 
             using (var client = ManagementClient.CreateNetworkClient())
             using (var context = new DevOpsFlexDbContext())
             {
                 await context.Components.OfType<AzureCloudService>().ReserveAllIps(client);
             }
+        }
+
+        /// <summary>
+        /// Tests the ProvisionAll on <see cref="AzureServiceBusNamespace"/> with real SQL Db data (seed).
+        /// </summary>
+        [TestMethod, TestCategory("Integration")]
+        public async Task Test_AzureServiceBusNamespace_ProvisionAll_End2End()
+        {
+            FlexDataConfiguration.Branch = "Main";
+            FlexDataConfiguration.Configuration = "MO";
+            FlexDataConfiguration.UseNaming(new LegacyFctSbNaming());
+
+            using (var client = ManagementClient.CreateServiceBusClient())
+            using (var context = new DevOpsFlexDbContext())
+            {
+                await context.Components.OfType<AzureServiceBusNamespace>().ProvisionAll(client);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Names the service bus in the same way we started naming in the early environment creation.
+    /// </summary>
+    public class LegacyFctSbNaming : IName<AzureServiceBusNamespace>
+    {
+        public string GetSlotName(AzureServiceBusNamespace component, string branch, string configuration)
+        {
+            return "nservicebus-" + configuration.ToLower();
         }
     }
 }
