e74881edf400667753eeb37049ee1dc1aebb0e6a
djfr <davidferreirarodrigues@gmail.com>
2015-04-28 11:37:04 +0100

    Added IP reservation abilities all the way from the NetworkClient to the DevOpsFlexDbContext.

--- src/DevOpsFlex.Common/DevOpsFlex.Azure.Management/DevOpsFlexDbContextExtensions.cs
+++ src/DevOpsFlex.Common/DevOpsFlex.Azure.Management/DevOpsFlexDbContextExtensions.cs
@@ -4,6 +4,7 @@
     using System.Threading.Tasks;
     using Data;
     using Microsoft.WindowsAzure.Management.Compute;
+    using Microsoft.WindowsAzure.Management.Network;
 
     /// <summary>
     /// Extends the <see cref="DevOpsFlexDbContext"/> with usefull extensions that the devopsflex
@@ -28,6 +29,25 @@
                                     async s =>
                                     {
                                         await client.CheckCreateCloudService(s);
+                                    });
+
+            await Task.WhenAll(tasks);
+        }
+
+        /// <summary>
+        /// Reserves all IPs for services in the <see cref="IQueryable"/> of <see cref="AzureCloudService"/> that need IP reservation.
+        /// </summary>
+        /// <param name="services">The list of <see cref="AzureCloudService"/> to reserve IPs for.</param>
+        /// <param name="client">The <see cref="ComputeManagementClient"/> that is performing the operation.</param>
+        /// <returns>The async <see cref="Task"/> wrapper.</returns>
+        public static async Task ReserveAllIps(this IQueryable<AzureCloudService> services, NetworkManagementClient client)
+        {
+            var tasks = services.Where(s => s.ReserveIp)
+                                .ToList()
+                                .Select(
+                                    async s =>
+                                    {
+                                        await client.CheckCreateReservedIp(s);
                                     });
 
             await Task.WhenAll(tasks);
---
+++ src/DevOpsFlex.Common/DevOpsFlex.Azure.Management/NetworkManagementClientExtensions.cs
@@ -0,0 +1,74 @@
+﻿namespace DevOpsFlex.Azure.Management
+{
+    using System.Diagnostics.Contracts;
+    using System.Threading.Tasks;
+    using System.Xml.XPath;
+    using Core;
+    using Data;
+    using Hyak.Common;
+    using Microsoft.WindowsAzure.Management.Network;
+    using Microsoft.WindowsAzure.Management.Network.Models;
+
+    /// <summary>
+    /// Extends the <see cref="NetworkManagementClient"/> with usefull extensions that the devopsflex
+    /// activities and commandlets need in order to achieve their execution.
+    /// </summary>
+    public static class NetworkManagementClientExtensions
+    {
+        /// <summary>
+        /// Checks for the existence of a specific Azure Reserved IP, if it doesn't exist it will create it.
+        /// </summary>
+        /// <param name="client">The <see cref="NetworkManagementClient"/> that is performing the operation.</param>
+        /// <param name="ipName">The name of the Reserved IP.</param>
+        /// <param name="location">The Azure location of the Reserved IP.</param>
+        /// <returns>The async <see cref="Task"/> wrapper.</returns>
+        public static async Task CheckCreateReservedIp(this NetworkManagementClient client, string ipName, string location)
+        {
+            Contract.Requires(client != null);
+            Contract.Requires(!string.IsNullOrWhiteSpace(ipName));
+            Contract.Requires(!string.IsNullOrWhiteSpace(location));
+
+            NetworkReservedIPGetResponse service = null;
+
+            try
+            {
+                service = await client.ReservedIPs.GetAsync(ipName);
+            }
+            catch (CloudException cex)
+            {
+                if (cex.Error.Code != "ResourceNotFound") throw;
+            }
+
+            if (service != null) return;
+
+            await client.ReservedIPs.CreateAsync(
+                new NetworkReservedIPCreateParameters
+                {
+                    Name = ipName,
+                    Location = location
+                });
+        }
+
+        /// <summary>
+        /// Checks for the existence of a specific Azure Reserved IP, if it doesn't exist it will create it.
+        /// </summary>
+        /// <param name="client">The <see cref="NetworkManagementClient"/> that is performing the operation.</param>
+        /// <param name="model">The DevOpsFlex rich model object that contains everything there is to know about this cloud service spec.</param>
+        /// <returns>The async <see cref="Task"/> wrapper.</returns>
+        public static async Task CheckCreateReservedIp(this NetworkManagementClient client, AzureCloudService model)
+        {
+            Contract.Requires(client != null);
+            Contract.Requires(model != null);
+            Contract.Requires(model.System != null);
+
+            var ipName = FlexDataConfiguration.GetNaming<AzureCloudService>()
+                                              .GetSlotName(
+                                                  model,
+                                                  FlexDataConfiguration.Branch,
+                                                  FlexDataConfiguration.Configuration)
+                         + "-rip";
+
+            await client.CheckCreateReservedIp(ipName, model.System.Location.GetEnumDescription());
+        }
+    }
+}
