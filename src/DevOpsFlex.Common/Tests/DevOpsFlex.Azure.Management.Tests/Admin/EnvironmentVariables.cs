f12c4864f30ee740eac8b23356c350154758ecdc
djfr <davidferreirarodrigues@gmail.com>
2015-04-29 11:17:29 +0100

    Added a utility test to create database environment variables around users and passwords.

---
+++ src/DevOpsFlex.Common/Tests/DevOpsFlex.Azure.Management.Tests/Admin/EnvironmentVariables.cs
@@ -0,0 +1,24 @@
+﻿namespace DevOpsFlex.Azure.Management.Tests.Admin
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Contains tests that deal with Administration tasks around environment variables.
+    /// </summary>
+    [TestClass]
+    public class EnvironmentVariables
+    {
+        /// <summary>
+        /// Sets all the required environment variables required for databases.
+        /// </summary>
+        [TestMethod, TestCategory("Administration")]
+        public void Set_Flex_Database_EnvironmentVariables()
+        {
+            Environment.SetEnvironmentVariable("FlexSaUser", "flexsa");
+            Environment.SetEnvironmentVariable("FlexSaPwd", "PASSWORD");
+            Environment.SetEnvironmentVariable("FlexAppUser", "flexapp");
+            Environment.SetEnvironmentVariable("FlexAppPwd", "PASSWORD");
+        }
+    }
+}
