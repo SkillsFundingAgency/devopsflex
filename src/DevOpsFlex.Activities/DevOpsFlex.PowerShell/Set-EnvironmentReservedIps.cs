a0280307bcec62e23ea146e1639a35b7403e575e
djfr <davidferreirarodrigues@gmail.com>
2015-07-24 16:59:14 +0100

    Initial draft for the set-environmentreservedips commandlet.

---
+++ src/DevOpsFlex.Activities/DevOpsFlex.PowerShell/Set-EnvironmentReservedIps.cs
@@ -0,0 +1,38 @@
+﻿namespace DevOpsFlex.PowerShell
+{
+    using System.Management.Automation;
+
+    /// <summary>
+    /// Set-EnvironmentReservedIps commandlet implementation.
+    /// </summary>
+    [Cmdlet(VerbsCommon.Set, "EnvironmentReservedIps")]
+    public class SetEnvironmentReservedIps : AsyncCmdlet
+    {
+        [Parameter(
+            Mandatory = true,
+            HelpMessage = "The Subscription Id that we are targetting for the deployments.")]
+        public string SubscriptionId { get; set; }
+
+        [Parameter(
+            Mandatory = true,
+            HelpMessage = "The absolute path to the Azure publish settings file.")]
+        public string SettingsPath { get; set; }
+
+        [Parameter(
+            Mandatory = true,
+            HelpMessage = "The name of the branch we want to target.")]
+        public string Branch { get; set; }
+
+        [Parameter(
+            Mandatory = true,
+            HelpMessage = "The .Net project configuration we want to target.")]
+        public string Configuration { get; set; }
+
+        /// <summary>
+        /// Processes the Set-EnvironmentReservedIps commandlet synchronously.
+        /// </summary>
+        protected override void ProcessRecord()
+        {
+        }
+    }
+}
