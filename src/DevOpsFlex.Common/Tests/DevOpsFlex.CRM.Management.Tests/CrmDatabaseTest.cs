9b3e5470bec055eba487f0514ddf1e88fff5be61
djfr <davidferreirarodrigues@gmail.com>
2015-08-11 11:09:16 +0100

    Added a CRM PowerShell solution draft to the repo.

---
+++ src/DevOpsFlex.Common/Tests/DevOpsFlex.CRM.Management.Tests/CrmDatabaseTest.cs
@@ -0,0 +1,55 @@
+﻿namespace Ciber.Xrm.Ci.Common.Tests
+{
+    using System;
+    //using System.Reactive;
+    //using System.Reactive.Subjects;
+    using DevOpsFlex.CRM.Management;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Contains tests that target the <see cref="CrmDatabase"/> SQL wrapper object.
+    /// </summary>
+    [TestClass]
+    public class CrmDatabaseTest
+    {
+        public TestContext TestContext { get; set; }
+
+        #region Integration
+
+        /// <summary>
+        /// Tests the dropping of a Database in SQL server.
+        /// The database needs to exist prior to running this Integration test.
+        /// </summary>
+        [TestMethod, TestCategory("IntegrationTest")]
+        public void Test_DropDatabase()
+        {
+            const string databaseName = "TestIntegration2_MSCRM";
+
+            //using (var eventStream = new Subject<BuildEvent>())
+            //using (var db = new CrmDatabase("fct-ci-crm-01.cloudapp.net", "integration_test", "Password.1234"))
+            //{
+            //    eventStream.Subscribe(e => TestContext.WriteLine("[{0}] [{1}] {2}", e.Type.ToString(), e.Importance.ToString(), e.Message));
+
+            //    db.DropDatabaseAsync(databaseName, eventStream.AsObserver()).Wait();
+            //}
+        }
+
+        /// <summary>
+        /// Tests if the master database exists.
+        /// </summary>
+        [TestMethod, TestCategory("IntegrationTest")]
+        public void Test_DatabaseExists()
+        {
+            const string databaseName = "master";
+
+            using (var db = new CrmDatabase("fct-ci-crm-01.cloudapp.net", "integration_test", "Password.1234"))
+            {
+                var exists = db.DatabaseExists(databaseName).Result;
+                Assert.IsTrue(exists);
+            }
+        }
+
+        #endregion
+
+    }
+}
