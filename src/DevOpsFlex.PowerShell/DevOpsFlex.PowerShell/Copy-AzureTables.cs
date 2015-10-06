494e800586da9629fc581a979da91c980eeb29ce
djfr <davidferreirarodrigues@gmail.com>
2015-10-06 12:22:04 +0100

    Bumped all packages in DevOpsFlex.PowerShell to the latest.

---
+++ src/DevOpsFlex.PowerShell/DevOpsFlex.PowerShell/Copy-AzureTables.cs
@@ -0,0 +1,19 @@
+﻿namespace DevOpsFlex.PowerShell
+{
+    using System.Management.Automation;
+    using Core;
+
+    /// <summary>
+    /// Copy-AzureTables commandlet implementation.
+    /// </summary>
+    [Cmdlet(VerbsCommon.Copy, "AzureTables")]
+    public class CopyAzureTables : AsyncCmdlet
+    {
+        /// <summary>
+        /// Processes the Copy-AzureTables commandlet synchronously.
+        /// </summary>
+        protected override void ProcessRecord()
+        {
+        }
+    }
+}
