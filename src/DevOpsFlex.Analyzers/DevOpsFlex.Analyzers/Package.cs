b752f862ec91bc2c1664ff3cbf2ca61fe33504b7
djfr <davidferreirarodrigues@gmail.com>
2015-12-01 16:30:01 +0000

    Adding missing packages from the previous commit.

--- src/DevOpsFlex.Analyzers/DevOpsFlex.Analyzers/Package.cs
+++ src/DevOpsFlex.Analyzers/DevOpsFlex.Analyzers/Package.cs
@@ -70,7 +70,7 @@ namespace DevOpsFlex.Analyzers
         /// <summary>
         /// Returns a hash code for the specified object.
         /// </summary>
-        /// <param name="package">The <see cref="T:System.Object"/> for which a hash code is to be returned.</param>
+        /// <param name="package">The <see cref="Package"/> for which a hash code is to be returned.</param>
         /// <returns>A hash code for the specified object.</returns>
         public int GetHashCode(Package package)
         {
