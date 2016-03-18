4d8b241f218f7140ca7733644b321e66137cb832
djfr <davidferreirarodrigues@gmail.com>
2016-03-18 16:48:28 +0000

    Small fix to the push deploy script.

--- src/DevOpsFlex.Environments.PowerShell/PushToMyGet.ps1
+++ src/DevOpsFlex.Environments.PowerShell/PushToMyGet.ps1
@@ -4,8 +4,9 @@ param
     [string] $ApiKey
 )
 
-$repo = Get-PSRepository -Name DevOpsFlex -ErrorAction SilentlyContinue
+Install-PackageProvider -Name NuGet -Force
 
+$repo = Get-PSRepository -Name DevOpsFlex -ErrorAction SilentlyContinue
 if($repo -eq $null) {
     Register-PSRepository -Name DevOpsFlex -SourceLocation "https://www.myget.org/F/devopsflex/api/v2" -InstallationPolicy Trusted
 }
