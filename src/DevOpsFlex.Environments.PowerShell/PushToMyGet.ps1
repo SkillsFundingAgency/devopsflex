0847c67f07bc71f43d459401e35efe88bb566203
djfr <davidferreirarodrigues@gmail.com>
2016-03-18 16:19:35 +0000

    Added a deployment script to push directly to a NuGet feed. Creates the DevOpsFlex PSRepository if it doesn't exist on the build rig.

---
+++ src/DevOpsFlex.Environments.PowerShell/PushToMyGet.ps1
@@ -0,0 +1,13 @@
+param
+(
+    [parameter(Mandatory=$true)]
+    [string] $ApiKey
+)
+
+$repo = Get-PSRepository -Name DevOpsFlex -ErrorAction SilentlyContinue
+
+if($repo -eq $null) {
+    Register-PSRepository -Name DevOpsFlex -SourceLocation "https://www.myget.org/F/devopsflex/api/v2" -InstallationPolicy Trusted
+}
+
+Publish-Module -Repository DevOpsFlex -Name .\DevOpsFlex.Environments.PowerShell\DevOpsFlex.Environments.PowerShell.psd1 -NuGetApiKey $ApiKey
